17f717b930efad38afaece6343396aa3f8afba34
KimHoGyun <khg010122@naver.com>
2025-11-13 16:23:38 +0900

    백엔드 1-1

---
+++ src/main/java/org/example/uteko_back/UtekoBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.uteko_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtekoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtekoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/uteko_back/UtekoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.uteko_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtekoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
